339406c247656cf320bbaa6877340a0f9933e390
Anurag2782 <116972643+Anurag2782@users.noreply.github.com>
2023-06-30 02:33:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = mysql.createConnection({
 });
 
 db.connect((err) => {
-    // if (err) {
-    //     throw err;
-    // }
+    if (err) {
+        throw err;
+    }
     console.log('Connected to database');
 });
 // }
